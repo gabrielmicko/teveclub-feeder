2663406c54184b43e258591656fb85c64ad8a72c
Gabriel Mičko <micsko@gmail.com>
2025-06-07 18:07:00 -0600

    hm

--- src/feed.js
+++ src/feed.js
@@ -16,39 +16,35 @@ function sleep() {
   return new Promise((resolve) => setTimeout(resolve, 3000));
 }
 
-async function feedCamel() {
-  if (camels.length === 0) {
-    process.exit();
+async function feedAllCamels() {
+  while (camels.length > 0) {
+    const camel = camels.pop();
+    const camelInstance = new Camel(camel);
+    const communication = new Communication();
+
+    try {
+      communication.setCamel(camelInstance);
+      console.log('Before getSession');
+      await communication.getSession();
+      await sleep();
+      console.log('Before auth');
+      await communication.auth();
+      await sleep();
+      console.log('Before feed');
+      await communication.feed();
+      await sleep();
+      console.log('Before teach');
+      await communication.teach();
+      await sleep();
+      console.log('Before lotto');
+      await communication.lotto();
+      await sleep();
+    } catch (error) {
+      console.error('Error while feeding camel:', error || 'Unknown error');
+    }
   }
 
-  const camel = camels.pop();
-  const camelInstance = new Camel(camel);
-  const communication = new Communication();
-
-  try {
-    communication.setCamel(camelInstance);
-    console.log('Before getSession');
-    await communication.getSession();
-    await sleep();
-    console.log('Before auth');
-    await communication.auth();
-    console.log('Before sleep, after auth');
-    await sleep();
-    console.log('Before feed');
-    await communication.feed();
-    await sleep();
-    console.log('Before teach');
-    await communication.teach();
-    await sleep();
-    console.log('Before lotto');
-    await communication.lotto();
-    await sleep();
-  } catch (error) {
-    console.error('Error while feeding camel:', error || 'Unknown error');
-  }
-
-  // Always call the next one regardless of success/failure
-  feedCamel();
+  process.exit();
 }
 
-feedCamel();
+feedAllCamels();
